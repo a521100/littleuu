dc55efbe1b30d20ccf8f046971432851e24edf72
Bingo You <33918081+a521100@users.noreply.github.com>
2018-11-07 10:52:29 +0800

    增加update功能

--- src/cn/web/dao/ProductDaoImpl.java
+++ src/cn/web/dao/ProductDaoImpl.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import javax.management.RuntimeErrorException;
-
 import cn.web.model.Product;
 import cn.web.utils.JdbcUtils;
 
@@ -16,11 +14,31 @@ public class ProductDaoImpl {
 		Product product = new Product();
 		product.setName("嗖嗖嗖");
 		product.setPrice(123.99);
-		//daoImpl.save(product);
-		//System.out.println(product);
-		daoImpl.delete(4);
+		product.setId(3);
+		product.setRemark("sfs");
+		// daoImpl.save(product);
+		// System.out.println(product);
+		// daoImpl.delete(4);
+		daoImpl.update(product);
 	}
-	
+
+	public int update(Product product) {
+		Connection conn = null;
+		PreparedStatement pre = null;
+		try {
+			conn = JdbcUtils.getConnection();
+			pre = conn.prepareStatement("update product set name = ? ,price = ? ,remark = ? where id = ?");
+			pre.setString(1, product.getName());
+			pre.setDouble(2, product.getPrice());
+			pre.setString(3, product.getRemark());
+			pre.setInt(4, product.getId());
+			return pre.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
 	public int delete(int id) {
 		Connection conn = null;
 		PreparedStatement pre = null;
@@ -30,11 +48,11 @@ public class ProductDaoImpl {
 			pre.setInt(1, id);
 			return pre.executeUpdate();
 		} catch (SQLException e) {
-			throw new RuntimeException(e) ;
+			throw new RuntimeException(e);
 		}
-		
+
 	}
-	
+
 	public int save(Product product) {
 		Connection conn = null;
 		PreparedStatement pre = null;
@@ -46,8 +64,8 @@ public class ProductDaoImpl {
 			pre.setString(3, product.getRemark());
 			return pre.executeUpdate();
 		} catch (SQLException e) {
-			throw new RuntimeException(e) ;
+			throw new RuntimeException(e);
 		}
-		
+
 	}
 }
